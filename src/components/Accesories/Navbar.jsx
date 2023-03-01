ca1379d3f827a1497ed79726e957500e16d452d5
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-03-01 14:16:04 -0300

    primer commit

---
+++ src/components/Accesories/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../../css/Accesories/Navbar.css";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <img src="/img/linux.png" alt="linuxtec" className="navbar-icon" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Distros
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <NavLink className="dropdown-item" to="debian">
+                    Debian
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="rpm">
+                    RPM
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="arch">
+                    Arch
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" href="#">
+                FAQ
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
